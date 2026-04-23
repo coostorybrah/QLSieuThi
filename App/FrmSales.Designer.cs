cc2a072806abb923043d061cb3884e903baca441
ChillGuy <veryreal021@gmail.com>
2026-04-23 12:50:39 +0700

    added sales page

---
+++ App/FrmSales.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace QLSieuThi
+{
+    partial class FrmSales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cartHeading = new System.Windows.Forms.Label();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            this.lblBarcode = new System.Windows.Forms.Label();
+            this.dgvCart = new System.Windows.Forms.DataGridView();
+            this.lblTotalText = new System.Windows.Forms.Label();
+            this.lblTotalValue = new System.Windows.Forms.Label();
+            this.btnCheckout = new System.Windows.Forms.Button();
+            this.btnClearCart = new System.Windows.Forms.Button();
+            this.btnAddToCart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cartHeading
+            // 
+            this.cartHeading.AutoSize = true;
+            this.cartHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.cartHeading.Location = new System.Drawing.Point(379, 18);
+            this.cartHeading.Name = "cartHeading";
+            this.cartHeading.Size = new System.Drawing.Size(161, 32);
+            this.cartHeading.TabIndex = 1;
+            this.cartHeading.Text = "GIỎ HÀNG";
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtBarcode.Location = new System.Drawing.Point(321, 97);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(363, 30);
+            this.txtBarcode.TabIndex = 0;
+            // 
+            // lblBarcode
+            // 
+            this.lblBarcode.AutoSize = true;
+            this.lblBarcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblBarcode.Location = new System.Drawing.Point(221, 100);
+            this.lblBarcode.Name = "lblBarcode";
+            this.lblBarcode.Size = new System.Drawing.Size(91, 25);
+            this.lblBarcode.TabIndex = 99;
+            this.lblBarcode.Text = "Barcode:";
+            // 
+            // dgvCart
+            // 
+            this.dgvCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCart.Location = new System.Drawing.Point(73, 166);
+            this.dgvCart.Name = "dgvCart";
+            this.dgvCart.RowHeadersWidth = 51;
+            this.dgvCart.RowTemplate.Height = 24;
+            this.dgvCart.Size = new System.Drawing.Size(793, 252);
+            this.dgvCart.TabIndex = 6;
+            // 
+            // lblTotalText
+            // 
+            this.lblTotalText.AutoSize = true;
+            this.lblTotalText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblTotalText.Location = new System.Drawing.Point(81, 447);
+            this.lblTotalText.Name = "lblTotalText";
+            this.lblTotalText.Size = new System.Drawing.Size(110, 25);
+            this.lblTotalText.TabIndex = 7;
+            this.lblTotalText.Text = "Tổng tiền:";
+            // 
+            // lblTotalValue
+            // 
+            this.lblTotalValue.AutoSize = true;
+            this.lblTotalValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblTotalValue.Location = new System.Drawing.Point(197, 447);
+            this.lblTotalValue.Name = "lblTotalValue";
+            this.lblTotalValue.Size = new System.Drawing.Size(24, 25);
+            this.lblTotalValue.TabIndex = 8;
+            this.lblTotalValue.Text = "0";
+            // 
+            // btnCheckout
+            // 
+            this.btnCheckout.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnCheckout.Location = new System.Drawing.Point(207, 532);
+            this.btnCheckout.Name = "btnCheckout";
+            this.btnCheckout.Size = new System.Drawing.Size(162, 46);
+            this.btnCheckout.TabIndex = 1;
+            this.btnCheckout.Text = "THANH TOÁN";
+            this.btnCheckout.UseVisualStyleBackColor = true;
+            // 
+            // btnClearCart
+            // 
+            this.btnClearCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnClearCart.Location = new System.Drawing.Point(574, 532);
+            this.btnClearCart.Name = "btnClearCart";
+            this.btnClearCart.Size = new System.Drawing.Size(163, 46);
+            this.btnClearCart.TabIndex = 2;
+            this.btnClearCart.Text = "HỦY ĐƠN";
+            this.btnClearCart.UseVisualStyleBackColor = true;
+            this.btnClearCart.Click += new System.EventHandler(this.btnClearCart_Click);
+            // 
+            // btnAddToCart
+            // 
+            this.btnAddToCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnAddToCart.Location = new System.Drawing.Point(703, 97);
+            this.btnAddToCart.Name = "btnAddToCart";
+            this.btnAddToCart.Size = new System.Drawing.Size(133, 30);
+            this.btnAddToCart.TabIndex = 101;
+            this.btnAddToCart.Text = "Thêm vào giỏ";
+            this.btnAddToCart.UseVisualStyleBackColor = true;
+            this.btnAddToCart.Click += new System.EventHandler(this.btnAddToCart_Click);
+            // 
+            // FrmSales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 664);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnAddToCart);
+            this.Controls.Add(this.btnClearCart);
+            this.Controls.Add(this.btnCheckout);
+            this.Controls.Add(this.lblTotalValue);
+            this.Controls.Add(this.lblTotalText);
+            this.Controls.Add(this.dgvCart);
+            this.Controls.Add(this.lblBarcode);
+            this.Controls.Add(this.txtBarcode);
+            this.Controls.Add(this.cartHeading);
+            this.Name = "FrmSales";
+            this.Load += new System.EventHandler(this.FrmSales_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cartHeading;
+        private System.Windows.Forms.TextBox txtBarcode;
+        private System.Windows.Forms.Label lblBarcode;
+        private System.Windows.Forms.DataGridView dgvCart;
+        private System.Windows.Forms.Label lblTotalText;
+        private System.Windows.Forms.Label lblTotalValue;
+        private System.Windows.Forms.Button btnCheckout;
+        private System.Windows.Forms.Button btnClearCart;
+        private System.Windows.Forms.Button btnAddToCart;
+    }
+}
---
+++ App/FrmSales.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLSieuThi
+{
+    public partial class FrmSales : Form
+    {
+        private BindingList<CartItem> cart = new BindingList<CartItem>();
+        private int oldQuantity;
+
+        public FrmSales()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmSales_Load(object sender, EventArgs e)
+        {
+            SetupCartGrid();
+
+            // DELEGATES
+            txtBarcode.KeyDown += txtBarcode_KeyDown;
+
+            cart.ListChanged += (s, ev) => UpdateTotal();
+
+            dgvCart.CellBeginEdit += dgvCart_CellBeginEdit;
+            dgvCart.CellEndEdit += dgvCart_CellEndEdit;
+            dgvCart.KeyDown += dgvCart_KeyDown;
+            dgvCart.CellContentClick += dgvCart_CellContentClick;
+        }
+
+        // BASIC FUNCTIONS
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            txtBarcode.Focus();
+        }
+
+        private void RefreshCart()
+        {
+            dgvCart.CommitEdit(DataGridViewDataErrorContexts.Commit);
+            dgvCart.Refresh();
+        }
+
+        private void UpdateTotal()
+        {
+            decimal total = cart.Sum(x => x.Total);
+            lblTotalValue.Text = total.ToString("N0") + " ₫";
+        }
+
+        // BARCODE LOGIC
+        private void txtBarcode_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                AddProductToCart(txtBarcode.Text.Trim());
+
+                txtBarcode.Clear();
+                txtBarcode.Focus();
+
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void AddProductToCart(string barcode)
+        {
+            if (string.IsNullOrWhiteSpace(barcode))
+                return;
+
+            try
+            {
+                DatabaseHelper db = new DatabaseHelper();
+
+                var result = db.ExecuteQuery(
+                    "sp_GetProductByBarcode",
+                    new SqlParameter("@Barcode", barcode)
+                );
+
+                if (result.Rows.Count == 0)
+                {
+                    System.Media.SystemSounds.Beep.Play();
+                    return;
+                }
+
+                var row = result.Rows[0];
+
+                int productId = Convert.ToInt32(row["ProductId"]);
+                string name = row["ProductName"].ToString();
+                decimal price = Convert.ToDecimal(row["SellingPrice"]);
+                int stock = Convert.ToInt32(row["StockQuantity"]);
+
+                // Check if already in cart
+                var existing = cart.FirstOrDefault(p => p.ProductId == productId);
+
+                if (existing != null)
+                {
+                    // Stock validation
+                    if (existing.Quantity + 1 > stock)
+                    {
+                        System.Media.SystemSounds.Beep.Play();
+                        return;
+                    }
+
+                    existing.Quantity++;
+                }
+                else
+                {
+                    // Out of stock
+                    if (stock <= 0)
+                    {
+                        System.Media.SystemSounds.Beep.Play();
+                        return;
+                    }
+
+                    cart.Add(new CartItem
+                    {
+                        ProductId = productId,
+                        ProductName = name,
+                        Price = price,
+                        Quantity = 1
+                    });
+                }
+
+                RefreshCart();
+                UpdateTotal();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error adding product: " + ex.Message);
+            }
+        }
+
+        // CART LOGIC
+        private void dgvCart_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Ignore header clicks
+            if (e.RowIndex < 0)
+                return;
+
+            // Check if it's the button column (last column)
+            if (dgvCart.Columns[e.ColumnIndex] is DataGridViewButtonColumn)
+            {
+                var item = dgvCart.Rows[e.RowIndex].DataBoundItem as CartItem;
+
+                if (item != null)
+                {
+                    cart.Remove(item);
+                }
+            }
+        }
+
+        private void dgvCart_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Deletes an entire row using keyboard
+            if (e.KeyCode == Keys.Delete && dgvCart.CurrentRow != null)
+            {
+                var item = dgvCart.CurrentRow.DataBoundItem as CartItem;
+                if (item != null)
+                {
+                    cart.Remove(item);
+                    RefreshCart();
+                    UpdateTotal();
+                }
+            }
+        }
+
+        private void dgvCart_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            if (dgvCart.Columns[e.ColumnIndex].DataPropertyName == "Quantity")
+            {
+                var item = cart[e.RowIndex];
+                oldQuantity = item.Quantity;
+            }
+        }
+
+        private void dgvCart_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvCart.Columns[e.ColumnIndex].DataPropertyName == "Quantity")
+            {
+                var item = cart[e.RowIndex];
+
+                var cellValue = dgvCart.Rows[e.RowIndex].Cells[e.ColumnIndex].Value?.ToString();
+
+                if (!int.TryParse(cellValue, out int value) || value < 0)
+                {
+                    item.Quantity = oldQuantity;
+                    return;
+                }
+
+                item.Quantity = value;
+
+                if (item.Quantity == 0)
+                {
+                    cart.Remove(item);
+                }
+            }
+
+            RefreshCart();
+            UpdateTotal();
+        }
+
+        private void SetupCartGrid()
+        {
+            dgvCart.AutoGenerateColumns = false;
+            dgvCart.DataSource = cart;
+
+            dgvCart.Columns.Clear();
+
+            // Product name
+            dgvCart.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "ProductName",
+                DataPropertyName = "ProductName",
+                HeaderText = "Product",
+                Width = 250
+            });
+            dgvCart.Columns["ProductName"].ReadOnly = true;
+
+            // Single unit price
+            dgvCart.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "Price",
+                DataPropertyName = "Price",
+                HeaderText = "Price",
+                Width = 100,
+                DefaultCellStyle = new DataGridViewCellStyle { Format = "N0" }
+            });
+            dgvCart.Columns["Price"].ReadOnly = true;
+
+            // Quantity of products
+            dgvCart.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "Quantity",
+                DataPropertyName = "Quantity",
+                HeaderText = "Qty",
+                Width = 80
+            });
+
+            // Total price of product group
+            dgvCart.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "Total",
+                DataPropertyName = "Total",
+                HeaderText = "Total",
+                Width = 120,
+                DefaultCellStyle = new DataGridViewCellStyle { Format = "N0" }
+            });
+            dgvCart.Columns["Total"].ReadOnly = true;
+
+            // Remove-product-group-from-list button
+            dgvCart.Columns.Add(new DataGridViewButtonColumn
+            {
+                Name = "btnDeleteRow",
+                HeaderText = "",
+                Text = "X",
+                UseColumnTextForButtonValue = true,
+                Width = 40
+            });
+
+            dgvCart.AllowUserToAddRows = false;
+            dgvCart.RowHeadersVisible = false;
+        }
+
+        private void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            AddProductToCart(txtBarcode.Text.Trim());
+            txtBarcode.Clear();
+            txtBarcode.Focus();
+        }
+
+        private void btnCheckout_Click(object sender, EventArgs e)
+        {
+            if (cart.Count == 0)
+            {
+                MessageBox.Show("Cart is empty.");
+                return;
+            }
+
+            try
+            {
+                DatabaseHelper db = new DatabaseHelper();
+
+                // 1. Create sale
+                var saleResult = db.ExecuteQuery(
+                    "sp_CreateSale",
+                    new SqlParameter("@EmployeeId", UserSession.EmployeeId),
+                    new SqlParameter("@CustomerId", DBNull.Value),
+                    new SqlParameter("@CustomerName", DBNull.Value),
+                    new SqlParameter("@CustomerPhone", DBNull.Value),
+                    new SqlParameter("@PaymentMethod", "Cash")
+                );
+
+                int saleId = Convert.ToInt32(saleResult.Rows[0]["SaleId"]);
+
+                // 2. Add all items
+                foreach (var item in cart)
+                {
+                    db.ExecuteNonQuery(
+                        "sp_AddSaleDetail",
+                        new SqlParameter("@SaleId", saleId),
+                        new SqlParameter("@ProductId", item.ProductId),
+                        new SqlParameter("@Quantity", item.Quantity)
+                    );
+                }
+
+                // 3. Finalize sale
+                db.ExecuteNonQuery(
+                    "sp_FinalizeSale",
+                    new SqlParameter("@SaleId", saleId),
+                    new SqlParameter("@Discount", 0)
+                );
+
+                // 4. Clear cart
+                cart.Clear();
+
+                MessageBox.Show("Thanh toán thành công!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Checkout failed: " + ex.Message);
+            }
+        }
+
+        private void btnClearCart_Click(object sender, EventArgs e)
+        {
+            cart.Clear();
+            RefreshCart();
+        }
+    }
+}
